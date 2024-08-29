6e7838b43f5c7dbdd16aa3adc0f013ddf8e80471
Dharshan-jm <dharshanjm@gmail.com>
2024-08-29 12:46:51 +0530

    commit_1_1.0 commit third

---
+++ project_1/src/test/java/project_demo/Testing_Panel.java
@@ -0,0 +1,13 @@
+package project_demo;
+
+import org.testng.annotations.Test;
+
+public class Testing_Panel {
+
+	@Test
+	public void testT()
+	{
+		System.out.println("Hello this is updated one");
+		System.out.println(12+" "+11);
+	}
+}
