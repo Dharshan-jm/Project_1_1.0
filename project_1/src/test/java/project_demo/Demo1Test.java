ee8c8a33db5e0fd7929432abe05344877ea2f62d
Dharshan-jm <dharshanjm@gmail.com>
2024-08-29 12:07:16 +0530

    commit_1_1.0 commit second

--- project_1/src/test/java/project_demo/Demo1Test.java
+++ project_1/src/test/java/project_demo/Demo1Test.java
@@ -1,14 +1,28 @@
 package project_demo;
 
+import java.io.IOException;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import com.castcast.crm.generic.fileutlity.FileUtility;
 
 import Base_Repository.BaseClass;
 
 public class Demo1Test extends BaseClass{
 	
 	 @Test
-	 public void demo1Test() {
+	 public void demo1Test() throws IOException {
+		 
+		 FileUtility fis=new FileUtility();
+
+		 String url=System.getProperty("url",fis.getDataFromPropertiesFile("Url"));
+		 String name=System.getProperty("name" ,fis.getDataFromPropertiesFile("username"));
+		 String password=System.getProperty("pwd" ,fis.getDataFromPropertiesFile("password"));
+		 
+		 System.out.println(url);
+		 System.out.println(name);
+		 System.out.println(password);
 		 
 		 System.out.println(10);
 		 Assert.assertEquals("hai", "hai");
